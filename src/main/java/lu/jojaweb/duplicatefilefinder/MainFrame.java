9d9fc5bad763c399e92ab25b6631c8459aa2cfcf
Joshua <Joshua@DESKTOP-S333HJD.mshome.net>
2022-04-01 17:30:37 +0200

    Added darkmode

--- src/main/java/lu/jojaweb/duplicatefilefinder/MainFrame.java
+++ src/main/java/lu/jojaweb/duplicatefilefinder/MainFrame.java
@@ -4,21 +4,31 @@
  */
 package lu.jojaweb.duplicatefilefinder;
 
-import java.awt.Container;
+import java.awt.Color;
 import java.awt.Desktop;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
 import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 import javax.swing.Timer;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.text.DefaultCaret;
 
 /**
@@ -31,14 +41,35 @@ public class MainFrame extends javax.swing.JFrame {
     Thread currentScan;
     private boolean duplicateFilePathListValueChangedPaused = false;
     private boolean originalFilePathListValueChangedPaused = false;
+    boolean darkmode = false;
 
     /**
      * Creates new form MainFrame
      */
     public MainFrame() {
+        File configFile = new File("settings.conf");
+        try ( BufferedReader in = new BufferedReader(new FileReader(configFile))) {
+            String line;
+            System.out.println("File:" + configFile.getAbsolutePath());
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+                if (line.contains("Theme")) {
+                    if (line.contains("Dark")) {
+                        setToDarkMode();
+                    }
+
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("No setting file found.");
+        } catch (IOException ex) {
+            System.out.println("An io exception occured while trying to read settings.conf");
+        }
 
         initComponents();
-
+        if (darkmode) {
+            jCheckBoxMenuItemDarkMode.setSelected(true);
+        }
         originalFilePathList.setDoubleBuffered(true);
         duplicateFilePathList.setDoubleBuffered(true);
         dff.initOutputField(jTextArea1);
@@ -59,8 +90,7 @@ public class MainFrame extends javax.swing.JFrame {
         });
         currentScan.start();
 
-        Container jTextAreaContainer = jTextArea1.getParent().getParent();
-        jTextAreaContainer.setVisible(false);
+        hideDetailsTextArea();
 
         //sync scrolls of both jlists
         originalFilePathListScrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {
@@ -102,7 +132,6 @@ public class MainFrame extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-        jSeparator1 = new javax.swing.JSeparator();
         jLabel1 = new javax.swing.JLabel();
         scannedFilesCountLabel = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -115,9 +144,14 @@ public class MainFrame extends javax.swing.JFrame {
         duplicateFilePathListScrollPane = new javax.swing.JScrollPane();
         duplicateFilePathList = new javax.swing.JList<>();
         jPanel1 = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jCheckBoxMenuItemDarkMode = new javax.swing.JCheckBoxMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("DFF");
 
         jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
@@ -141,7 +175,7 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
 
-        jSplitPane2.setDividerLocation(100);
+        jSplitPane2.setDividerLocation(200);
         jSplitPane2.setDividerSize(0);
         jSplitPane2.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
         jSplitPane2.setResizeWeight(0.1);
@@ -189,31 +223,54 @@ public class MainFrame extends javax.swing.JFrame {
 
         jSplitPane2.setBottomComponent(jSplitPane1);
 
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("Filter, Choose folders/drives, exclude folders/drives go here");
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 1008, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1008, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 36, Short.MAX_VALUE))
         );
 
         jSplitPane2.setLeftComponent(jPanel1);
+
+        jMenu1.setText("File");
+        jMenu1.setEnabled(false);
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("View");
+
+        jCheckBoxMenuItemDarkMode.setText("Dark Mode");
+        jCheckBoxMenuItemDarkMode.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jCheckBoxMenuItemDarkModeActionPerformed(evt);
+            }
+        });
+        jMenu2.add(jCheckBoxMenuItemDarkMode);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1)
-            .addComponent(jSeparator1)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
@@ -227,11 +284,12 @@ public class MainFrame extends javax.swing.JFrame {
                 .addComponent(jButton1)
                 .addContainerGap())
             .addComponent(jSplitPane2)
+            .addComponent(jScrollPane1)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(jSplitPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 530, Short.MAX_VALUE)
+                .addComponent(jSplitPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
@@ -240,9 +298,7 @@ public class MainFrame extends javax.swing.JFrame {
                     .addComponent(duplicateFilesCountLabel)
                     .addComponent(jButton1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
@@ -251,13 +307,11 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         dff.toggleShowDetails();
-        Container jTextAreaContainer = jTextArea1.getParent().getParent();
-        if (!jTextAreaContainer.isVisible()) {
-            jTextAreaContainer.setVisible(true);
-            setSize(getWidth(), getHeight() + 1);
+        if (!jScrollPane1.isVisible()) {
+            jScrollPane1.setVisible(true);
+            SwingUtilities.updateComponentTreeUI(this);
         } else {
-            jTextAreaContainer.setVisible(false);
-            setSize(getWidth(), getHeight() - 1);
+            hideDetailsTextArea();
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -364,6 +418,34 @@ public class MainFrame extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_originalFilePathListKeyPressed
 
+    private void jCheckBoxMenuItemDarkModeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItemDarkModeActionPerformed
+        JCheckBoxMenuItem darkModeJCheckbox = (JCheckBoxMenuItem) evt.getSource();
+        if (darkModeJCheckbox.isSelected()) {
+            try ( FileWriter out = new FileWriter(new File("settings.conf"))) {
+                out.append("Theme: Dark");
+                if (!darkmode) {
+                    JOptionPane.showMessageDialog(null, "Darkmode will be activated once the program is restarted.", "Info", JOptionPane.INFORMATION_MESSAGE);
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("No setting file found.");
+            } catch (IOException ex) {
+                System.out.println("An io exception occured while trying to read settings.conf");
+            }
+        } else {
+            try ( FileWriter out = new FileWriter(new File("settings.conf"))) {
+                out.append("Theme: Light");
+                if (darkmode) {
+                    JOptionPane.showMessageDialog(null, "Darkmode will be deactivated once the program is restarted.", "Info", JOptionPane.INFORMATION_MESSAGE);
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("No setting file found.");
+            } catch (IOException ex) {
+                System.out.println("An io exception occured while trying to read settings.conf");
+            }
+        }
+        SwingUtilities.updateComponentTreeUI(this);
+    }//GEN-LAST:event_jCheckBoxMenuItemDarkModeActionPerformed
+
     private void openFileOrFolder(String path) {
 
         if ((new File(path)).exists()) {
@@ -385,27 +467,22 @@ public class MainFrame extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
+        for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                try {
+                    UIManager.setLookAndFeel(info.getClassName());
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InstantiationException ex) {
+                    Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (UnsupportedLookAndFeelException ex) {
+                    Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
                 }
+                break;
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -420,12 +497,15 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JScrollPane duplicateFilePathListScrollPane;
     private javax.swing.JLabel duplicateFilesCountLabel;
     private javax.swing.JButton jButton1;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItemDarkMode;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSplitPane jSplitPane1;
     private javax.swing.JSplitPane jSplitPane2;
     private javax.swing.JTextArea jTextArea1;
@@ -433,4 +513,32 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JScrollPane originalFilePathListScrollPane;
     private javax.swing.JLabel scannedFilesCountLabel;
     // End of variables declaration//GEN-END:variables
+
+    private void setToDarkMode() {
+        //https://docs.oracle.com/javase/tutorial/uiswing/lookandfeel/_nimbusDefaults.html#primary
+        UIManager.put("nimbusBase", new Color(0, 0, 0)); //SCROLLBAR CARRET color
+
+        UIManager.put("nimbusBlueGrey", new Color(30, 33, 35)); //shine effect over components
+        UIManager.put("nimbusSelectionBackground", new Color(22, 58, 64)); // background color of selected text
+        UIManager.put("nimbusSelectedText", new Color(220, 220, 220)); // foreground color of selected text
+
+        UIManager.put("MenuBar:Menu[Enabled].textForeground", new Color(200, 200, 200)); //Menu text color
+        UIManager.put("MenuBar:Menu[Disabled].textForeground", new Color(120, 120, 120)); //Menu text color
+
+        UIManager.put("TextArea.background", new Color(70, 73, 75)); //JTextArea background color
+
+        UIManager.put("List.background", new Color(70, 73, 75));//JList background color
+
+        //UIManager.put("nimbusInfoBlue", new Color(255, 0, 0)); //?
+        UIManager.put("nimbusFocus", new Color(200, 200, 200));// shadow around button and other selected components
+        UIManager.put("control", new Color(60, 63, 65)); // window background [60,63,65] 
+        UIManager.put("text", new Color(200, 200, 200));//[187,187,187]
+        darkmode = true;
+    }
+
+    private void hideDetailsTextArea() {
+        jScrollPane1.setVisible(false);
+        SwingUtilities.updateComponentTreeUI(this);
+    }
+
 }
